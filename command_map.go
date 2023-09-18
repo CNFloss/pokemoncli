192b32aecb1ab753918f8ab428fb60ac54c78d12
cnfloss <jsdevelopercolin@gmail.com>
2023-09-18 18:46:30 -0400

    fix map command logic

--- command_map.go
+++ command_map.go
@@ -11,38 +11,40 @@ import (
 var cache pokecache.Cache = pokecache.NewCache(time.Hour)
 
 func commandMap(config *pokeapi.Client) error {
+	// declare empty struct for unmarshaling JSON
 	locationAreas := pokeapi.LocationAreaResp{}
+	// check if this is the first API call
 	if config.Next == nil {
-		fmt.Println(config.Next)
 		firstReq := pokeapi.BaseURL + "/location"
 		config.Next = &firstReq
+	} else {
+		// check if API call is cached
+		dat, ok := cache.Get(*config.Next)
+		if ok {
+			err := json.Unmarshal(dat, &locationAreas)
+			if err != nil {
+				return err
+			}
+			config.Next = locationAreas.Next
+			if locationAreas.Previous != nil {
+				config.Previous = locationAreas.Previous
+			}
+			for _, loc := range locationAreas.Results {
+				fmt.Printf("%s\n", loc.Name)
+			}
+			return nil
+		}
 	}
-	dat, ok := cache.Get(*config.Next)
-	if ok {
-		err := json.Unmarshal(dat, &locationAreas)
-		if err != nil {
-			return err
-		}
-		config.Next = locationAreas.Next
-		if locationAreas.Previous != nil {
-			config.Previous = locationAreas.Previous
-		}
-		for _, loc := range locationAreas.Results {
-			fmt.Printf("%s\n", loc.Name)
-		}
-		return nil
-	}
+	// make API call, unmarshal json, add to cache set next
 	results, err := pokeapi.ListLocationAreas(config.Next)
 	if err != nil {
 		fmt.Printf("%s", err)
 		return nil
 	}
-
 	err = json.Unmarshal(results, &locationAreas)
 	if err != nil {
 		return err
 	}
-
 	cache.Add(*config.Next, results)
 	config.Next = locationAreas.Next
 	if locationAreas.Previous != nil {
@@ -55,25 +57,26 @@ func commandMap(config *pokeapi.Client) error {
 }
 
 func commandMapB(config *pokeapi.Client) error {
+	locationAreas := pokeapi.LocationAreaResp{}
 	if config.Previous == nil {
 		fmt.Println("No previous locations")
 		return nil
-	}
-	locationAreas := pokeapi.LocationAreaResp{}
-	dat, ok := cache.Get(*config.Previous)
-	if ok {
-		err := json.Unmarshal(dat, &locationAreas)
-		if err != nil {
-			return err
+	} else {
+		dat, ok := cache.Get(*config.Previous)
+		if ok {
+			err := json.Unmarshal(dat, &locationAreas)
+			if err != nil {
+				return err
+			}
+			config.Next = locationAreas.Next
+			if locationAreas.Previous != nil {
+				config.Previous = locationAreas.Previous
+			}
+			for _, loc := range locationAreas.Results {
+				fmt.Printf("%s\n", loc.Name)
+			}
+			return nil
 		}
-		config.Next = locationAreas.Next
-		if locationAreas.Previous != nil {
-			config.Previous = locationAreas.Previous
-		}
-		for _, loc := range locationAreas.Results {
-			fmt.Printf("%s\n", loc.Name)
-		}
-		return nil
 	}
 	results, err := pokeapi.ListLocationAreas(config.Previous)
 	if err != nil {
